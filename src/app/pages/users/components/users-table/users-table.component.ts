8e7e4b7f2fdb2ff9550cba906391e35a1a9d26c6
sniperdev <igorspychalaa@gmail.com>
2024-01-02 13:25:39 +0100

    <feat> sort table

--- src/app/pages/users/components/users-table/users-table.component.ts
+++ src/app/pages/users/components/users-table/users-table.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { User } from '../../../../shared/interfaces/user.interface';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
   styleUrls: ['./users-table.component.scss'],
 })
-export class UsersTableComponent {
-  @Input() users: User[] | null = [];
+export class UsersTableComponent implements AfterViewInit {
+  // @Input() users: User[] | null = [];
+  @Input() set users(users: User[] | null) {
+    this.dataSource = new MatTableDataSource<User>(users as User[]);
+    this.dataSource.sort = this.sort;
+    // console.log(this.dataSource, users);
+  }
+  @ViewChild(MatSort) sort!: MatSort;
+
+  dataSource = new MatTableDataSource<User>(this.users as User[]);
+
   protected displayedColumns: string[] = [
     'name',
     'lastname',
     'country',
     'gender',
   ];
+
+  protected getName(user: User) {
+    return user.name.first;
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 }
